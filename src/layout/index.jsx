784f844901305253c563af5e0699b826fc92c6de
Alexander Trost <AlexRTrost@gmail.com>
2019-03-13 07:58:32 -0400

    Add outer wrapper for layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import Helmet from "react-helmet";
+import styled from "styled-components";
 import config from "../../data/SiteConfig";
 import "./index.css";
 
+const Outer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  max-width: 1024px;
+`;
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div>
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-        </Helmet>
-        {children}
-      </div>
+      <Outer>
+        <Inner>
+          <Helmet>
+            <meta name="description" content={config.siteDescription} />
+          </Helmet>
+          {children}
+        </Inner>
+      </Outer>
     );
   }
 }
