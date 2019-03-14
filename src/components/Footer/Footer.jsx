c7baad436e0ae916ece76b584a8f59ae6d33d93f
Alexander Trost <AlexRTrost@gmail.com>
2019-03-13 22:20:12 -0400

    Add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,103 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
-import UserLinks from "../UserLinks/UserLinks";
 import "./Footer.css";
+import styled from "styled-components";
+import ellePicture from "../../../static/images/elle.jpg";
+import alexPicture from "../../../static/images/alex.jpg";
+
+const FooterComponent = styled.footer`
+  width: 100%;
+  border-top: 1px solid #eee;
+  background: rgba(250, 250, 250, 186);
+  padding: 2rem;
+  padding-bottom: 1rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  .copyright {
+    color: #aaa;
+    flex: 1 100%;
+    font-size: 14px;
+    letter-spacing: 0.3px;
+    text-align: center;
+    margin-top:2rem;
+  }
+  @media all and (max-width: 600px) {
+    padding: 1rem;
+  }
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  padding: 1rem 3rem;
+  @media all and (max-width: 600px) {
+    padding: 1rem 0.5rem;
+  }
+`;
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  width: 125px;
+  height: 125px;
+  box-shadow: 1px 2px 1px rgba(0, 0, 0, 0.2);
+  margin-right: 1rem;
+
+  @media all and (max-width: 600px) {
+    width: 100px;
+    height: 100px;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+
+  justify-content: center;
+
+  .name {
+    font-size: 22px;
+    margin: 0;
+    color: #626262;
+    line-height: 1.2;
+  }
+  .title {
+    font-size: 16px;
+    color: #9a9898;
+    font-weight: 400;
+    margin: 0;
+    margin-bottom: 6px;
+    line-height: 1;
+  }
+`;
 
 class Footer extends Component {
   render() {
-    const { config } = this.props;
-    const url = config.siteRss;
-    const { copyright } = config;
-    if (!copyright) {
-      return null;
-    }
     return (
-      <footer className="footer">
-        <UserLinks config={config} labeled />
-        <div className="notice-container">
-          <h4>{copyright}</h4>
+      <FooterComponent>
+        <Profile>
+          <Avatar src={ellePicture} />
+          <TextWrapper>
+            <h4 className="name">Elle Trost</h4>
+            <h5 className="title">Graphic Designer</h5>
+            <a href="https://twitter.com/lovelettersco">@lovelettersco</a>
+            <a href="https://www.loveletterspaper.com/">Website</a>
+          </TextWrapper>
+        </Profile>
 
-          <Link to={url}>
-            <button>Subscribe</button>
-          </Link>
-          <h4>
-            Based on{" "}
-            <a href="https://github.com/Vagr9K/gatsby-advanced-starter">
-              Gatsby Advanced Starter
-            </a>.
-          </h4>
-        </div>
-      </footer>
+        <Profile>
+          <Avatar src={alexPicture} />
+          <TextWrapper>
+            <h4 className="name">Alex Trost</h4>
+            <h5 className="title">Web Developer</h5>
+            <a href="https://twitter.com/mistertrost">@mistertrost</a>
+            <a href="https://www.atrost.com">Website</a>
+          </TextWrapper>
+        </Profile>
+
+        <p className="copyright">Copyright ©2019 - The Overlap</p>
+      </FooterComponent>
     );
   }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,19 +7,38 @@ import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import Countdown from "../components/Countdown";
+import Player from "../components/Player";
+import Footer from "../components/Footer/Footer";
+import logo from "../../static/images/overlapLogoNoTagline.svg";
+
+const LogoHeader = styled.div`
+  margin: 1rem;
+`;
+
+const Logo = styled.img`
+  width: 100%;
+  max-width: 90%;
+`;
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
-      <Layout>
-        <div className="index-container">
-          <Helmet title={config.siteTitle} />
-          <SEO />
-          <Countdown />
-          {/* <PostListing postEdges={postEdges} /> */}
-        </div>
-      </Layout>
+      <>
+        <Layout>
+          <div className="index-container">
+            <Helmet title={config.siteTitle} />
+            <SEO />
+            {/* <LogoHeader>
+            <Logo src={logo} alt="Logo" width="500px" />
+          </LogoHeader>
+          <Player track={this.props.data.allFeedOverlapPodcast.edges[0].node} /> */}
+            {/* <PostListing postEdges={this.props.data.allFeedOverlapPodcast.edges} />  */}
+            <Countdown />
+          </div>
+        </Layout>
+        <Footer />
+      </>
     );
   }
 }
@@ -29,6 +48,26 @@ export default Index;
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query IndexQuery {
+    allFeedOverlapPodcast {
+      edges {
+        node {
+          title
+          link
+          content {
+            encoded
+          }
+          enclosure {
+            url
+            length
+            type
+          }
+        }
+      }
+    }
+    feedOverlapPodcast {
+      title
+      link
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [fields___date], order: DESC }
