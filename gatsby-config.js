2013b9600386e48b3008553ae118165ae7e667cc
Alexander Trost <AlexRTrost@gmail.com>
2019-03-26 08:25:29 -0400

    Add plugin to remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,7 +109,7 @@ module.exports = {
         ]
       }
     },
-    // "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     {
       resolve: "gatsby-plugin-feed",
       options: {
