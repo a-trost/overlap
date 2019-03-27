f91c838c64444d2149fc2a22914242a23a914ec9
Alexander Trost <AlexRTrost@gmail.com>
2019-03-26 20:43:16 -0400

    Remove email field

--- src/components/EmailForm/index.jsx
+++ src/components/EmailForm/index.jsx
@@ -17,10 +17,6 @@ const Contact = props => (
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" required />
           </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" required />
-          </div>
           <div className="field">
             <label htmlFor="message">Message</label>
             <textarea name="message" id="message" rows="6" required />
