2d36b1d1cd68496137e7010cf851b951750a6d61
Alexander Trost <AlexRTrost@gmail.com>
2019-03-26 20:29:09 -0400

    Revert to broken form

--- src/components/EmailModal/index.jsx
+++ src/components/EmailModal/index.jsx
@@ -1,58 +1,157 @@
-import React from 'react'
+import React, { Component } from "react";
+import {
+  Button,
+  Header,
+  Form,
+  Icon,
+  Modal,
+  Input,
+  Message,
+  TextArea
+} from "semantic-ui-react";
+import styled from "styled-components";
 
-const Contact = (props) => (
-    <section id="contact">
-        <div className="inner">
-            <section>
-                <form name="contact" method="post" action="/thanks" data-netlify="true" data-netlify-honeypot="bot-field">
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-                    <input type="hidden" name="bot-field" />
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 1.5;
+`;
 
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" required/>
-                    </div>
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" required/>
-                    </div>
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6" required></textarea>
-                    </div>
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
-                </form>
-            </section>
-            <section className="split">
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-envelope"></span>
-                        <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-phone"></span>
-                        <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
-                    </div>
-                </section>
-                <section>
-                    <div className="contact-method">
-                        <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
-                        United States of America</span>
-                    </div>
-                </section>
-            </section>
-        </div>
-    </section>
-)
+export default class EmailModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen: false, submitted: false, error: "", name:"", question:"" };
+  }
 
-export default Contact
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => this.setState({ submitted: true }))
+      .catch(error => alert(error));
+    };
+
+  handleOpen = () => {
+    this.setState({ modalOpen: true, submitted: false });
+  };
+
+  handleClose = () => this.setState({ modalOpen: false });
+
+  render() {
+    const { submitted } = this.state;
+    return (
+      <Modal
+        dimmer="inverted"
+        trigger={
+          <Button
+            onClick={this.handleOpen}
+            className="footerButton"
+            size="small"
+            basic
+            color="red"
+          >
+            <Icon name="paper plane" />
+            Send us your questions
+          </Button>
+        }
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+      >
+        <Header icon="paper plane" content="Send us your questions!" />
+        <Modal.Content>
+          {!submitted && (
+            <>
+              <Description>
+                If you have any questions about design or development, ask away!
+                We'll try to get to it on an upcoming episode of the podcast.
+              </Description>
+
+              <Description>
+                While there are no stupid questions, there's a decent chance
+                we'll give a stupid answer.
+              </Description>
+            </>
+          )}
+          {submitted && (
+            <Message
+              success
+              header="Question Sent!"
+              content="Hopefully we'll be able to get to your question on the podcast. Thanks so much for taking the time to ask it!"
+            />
+          )}
+          <form
+            name="contact"
+            method="post"
+            action="/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+            netlify="true"
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
+            <Form.Field
+              id="form-input-control-name"
+              control={Input}
+              label="Name"
+
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              required
+            />
+
+            <Form.Field
+              id="form-textarea-control-name"
+              control={TextArea}
+              label="Question"
+              placeholder="Your Question"
+              name="question"
+              onChange={this.handleChange}
+            />
+            {!submitted && (
+              <Button color="blue" type="submit" animated>
+                <Button.Content visible>Send</Button.Content>
+                <Button.Content hidden>
+                  <Icon name="paper plane" />
+                </Button.Content>
+              </Button>
+            )}
+            {submitted && (
+              <Button type="button" disabled>
+                Sent!
+              </Button>
+            )}
+          </form>
+        </Modal.Content>
+        {/* <Modal.Actions>
+          <Button color="green" onClick={this.handleClose} inverted>
+            <Icon name="checkmark" /> Got it
+          </Button>
+        </Modal.Actions> */}
+      </Modal>
+    );
+  }
+}
