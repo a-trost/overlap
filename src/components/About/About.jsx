64ff27aa0b85b1c614a89fcf147165102fe84efc
Alexander Trost <AlexRTrost@gmail.com>
2019-04-28 16:11:45 -0400

    Update About Page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import "./About.css";
+import styled from "styled-components";
+
+const StyledAbout = styled.section`
+  grid-area: about;
+  display:flex;
+  flex-flow: column nowrap
+`;
 
 class About extends Component {
   render() {
     return (
-      <div className="about">
-        <h1>
-          Edit About component or pages/about.jsx to include your information.
-        </h1>
-      </div>
+      <StyledAbout>
+        <h4>About</h4>
+        <p>
+          The Overlap is a podcast that explores the intersection of Design and
+          Front End Development.
+        </p>
+        <p>
+          On the show we discuss all things design and development in a fun and
+          easy-to-listen-to way.
+        </p>
+        <p>
+          Some topics we cover are typography, React, productivity, focus,
+          designing for print, freelancing tips, and much more.
+        </p>
+      </StyledAbout>
     );
   }
 }
